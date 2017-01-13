acd8972f4499f548a6bc65c90c36ef9168f5626a
Andrew Scagnelli <andrew.scagnelli@gmail.com>
2017-01-13 12:48:03 -0500

    Making entries fancier, glomming into one

--- day_one_git_messages.sh
+++ day_one_git_messages.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env sh
-repos='path/to/repo1 /path/to/repo2 ...'
+repos='/path/to/repo ...'
+user='git username'
 
+journal=""
+tags=""
+date=`date +%Y-%m-%d`
+entry="# Commit log for $date"
+hasLog=false
 for repoPath in $repos
 do
     cd $repoPath
-    logs=`git log --since=6am`
+    logs=`GIT_PAGER=cat git log --since=6am --author=$user --pretty=tformat:"${formatString}"`
     if [ -n "$logs" ]; then
         echo "Found logs @ $repoPath"
-        echo "Commits for $repoPath\\n $logs" | dayone -d="today" new
+        entry+="\\n ## $repoPath \\n "
+        entry+=`echo "$logs"`
+        hasLog=true
+    else
+        echo "No commits found for $repoPath"
     fi
 done
+if [ "$hasLog" = true ]; then
+    echo "Logs found; adding entry"
+    echo "$entry" | dayone2 -d="today" -t="$tags" -j="$journal" new
+else
+    echo "No logs found; not adding entry"
+fi
